eacc7361b126d5d817b95e1d387c43f3666f8cc5
abdou-bernous <a_barnous@esi.dz>
2018-01-09 19:39:30 +0100

    Ajouter un champ texte nommé « exercise » sur l’objet sale.order plus autre champs comme example

--- models/models.py
+++ models/models.py
@@ -2,14 +2,13 @@
 
 from odoo import models, fields, api
 
-# class exercice(models.Model):
-#     _name = 'exercice.exercice'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+# Ajouter un champ texte nommé « exercise » sur l’objet sale.order
+
+class sale_order(models.Model):
+    _inherit = "sale.order"
+    exercise = fields.Text()
+    final_customer_id = fields.Many2one(comodel_name="res.partner", string="Client Final", required=False,domain="[('active','=',True)]" )
+    to_validate = fields.Boolean("Prêt à valider", default=False, copie=False)
+    remise_client = fields.Float(string="Remise (%)",  required=False, readonly= True)
+    delai_livraison = fields.Float(string="Delai de Livraison",  required=False,)
