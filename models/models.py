875a4e81592c24e10fb00906ed010cc4ccbb4c47
abdou-bernous <a_barnous@esi.dz>
2018-01-09 21:26:22 +0100

    rajouter la traduction via un fichier .po (italien)

--- models/models.py
+++ models/models.py
@@ -8,7 +8,6 @@ from odoo import models, fields, api
 class sale_order(models.Model):
     _inherit = "sale.order"
     exercise = fields.Text()
-    final_customer_id = fields.Many2one(comodel_name="res.partner", string="Client Final", required=False,domain="[('active','=',True)]" )
     to_validate = fields.Boolean("Prêt à valider", default=False, copie=False)
     remise_client = fields.Float(string="Remise (%)",  required=False, readonly= True)
     delai_livraison = fields.Float(string="Delai de Livraison",  required=False,)
