9366657a8fc244a5007e9a8dcf6316e2630d42ba
abdou-bernous <a_barnous@esi.dz>
2018-01-09 19:35:50 +0100

    modification du ficheir manifest pour l'exercise

--- __manifest__.py
+++ __manifest__.py
@@ -3,33 +3,32 @@
     'name': "Exercice",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        exercice de test""",
 
     'description': """
-        Long description of module's purpose
+        Ce module ajoute des modifications  pour le process de vente
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "bernous abd sallem",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/sale_order_view.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+      #  'demo/demo.xml',
     ],
 }
