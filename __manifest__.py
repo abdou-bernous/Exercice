4796f2a134d0a14abac6dfe27389a1ad68f08946
abdou-bernous <a_barnous@esi.dz>
2018-01-11 00:37:47 +0100

    correction des remarques

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Exercice",
-
     'summary': """
         exercice de test""",
-
     'description': """
-        Ce module ajoute des modifications  pour le process de vente
-    """,
-
+        Ce module ajoute des modifications  pour le process de vente    """,
     'author': "bernous abd sallem",
     'website': "",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'sale',
     'version': '0.1',
-
     # any module necessary for this one to work correctly
     'depends': ['base','sale'],
-
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/sale_order_view.xml',
-        'report/sale_order_rapport.xml',
-    ],
+        'report/sale_order_rapport.xml'],
     # only loaded in demonstration mode
-    'demo': [
-      #  'demo/demo.xml',
-    ],
+    'demo': [      #  'demo/demo.xml',
+            ],
 }
--- models/models.py
+++ models/models.py
@@ -2,12 +2,11 @@
 
 from odoo import models, fields, api
 
-
 # Ajouter un champ texte nommé « exercise » sur l’objet sale.order
 
-class sale_order(models.Model):
+class saleorder(models.Model):
     _inherit = "sale.order"
-    exercise = fields.Text()
-    to_validate = fields.Boolean("Prêt à valider", default=False, copie=False)
-    remise_client = fields.Float(string="Remise (%)",  required=False, readonly= True)
-    delai_livraison = fields.Float(string="Delai de Livraison",  required=False,)
+    exercise = fields.Text(string ="exercise")
+    to_validate = fields.Boolean("Prêt à valider", default=False)
+    customer_discount = fields.Float(string="Remise (%)",  required=False)
+    delivery_time = fields.Float(string="Delai de Livraison",  required=False)
